b8aa0cc34ea480871f24d3fc810cfa4ed9ea02d3
wlstj <22020306jinseoyeon@gmail.com>
2024-12-06 01:39:05 +0900

    build: initial setting

---
+++ src/main/java/com/example/momowas/MomoWasApplication.java
@@ -0,0 +1,13 @@
+package com.example.momowas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomoWasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/momowas/MomoWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.momowas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
