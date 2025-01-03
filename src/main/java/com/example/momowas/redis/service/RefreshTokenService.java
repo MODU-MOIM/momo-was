41364b2b48ca40f35b183c065874f730998d89cf
wlstj <22020306jinseoyeon@gmail.com>
2025-01-03 17:21:28 +0900

    build: google setting

--- src/main/java/com/example/momowas/redis/service/RefreshTokenService.java
+++ src/main/java/com/example/momowas/redis/service/RefreshTokenService.java
@@ -10,9 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
