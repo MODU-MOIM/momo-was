85a1b870bb66debf5769aca3fd76f234d6624c63
Chaeyoon Kim <cy6kimm@gmail.com>
2025-01-12 03:14:54 +0900

    feat: SecurityConfig에 preAuthorize 사용을 위한 애노테이션 추가

--- src/main/java/com/example/momowas/config/SecurityConfig.java
+++ src/main/java/com/example/momowas/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,6 +26,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity(debug = true)
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
 
