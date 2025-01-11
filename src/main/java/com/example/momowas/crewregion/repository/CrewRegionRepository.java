acc46a754dba353273639a213fc93fc8cc041f40
Chaeyoon Kim <cy6kimm@gmail.com>
2025-01-11 19:09:08 +0900

    feat: 특정 크루의 지역 조회 기능 구현

--- src/main/java/com/example/momowas/crewregion/repository/CrewRegionRepository.java
+++ src/main/java/com/example/momowas/crewregion/repository/CrewRegionRepository.java
@@ -4,6 +4,9 @@ import com.example.momowas.crewregion.domain.CrewRegion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CrewRegionRepository extends JpaRepository<CrewRegion, Long> {
+    List<CrewRegion> findByCrewId(Long crewId);
 }
--- src/main/java/com/example/momowas/crewregion/service/CrewRegionService.java
+++ src/main/java/com/example/momowas/crewregion/service/CrewRegionService.java
@@ -4,7 +4,7 @@ import com.example.momowas.crew.domain.Crew;
 import com.example.momowas.crewregion.domain.CrewRegion;
 import com.example.momowas.crewregion.repository.CrewRegionRepository;
 import com.example.momowas.region.domain.Region;
-import com.example.momowas.region.dto.RegionReqDto;
+import com.example.momowas.region.dto.RegionDto;
 import com.example.momowas.region.service.RegionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,16 +19,25 @@ public class CrewRegionService {
     private final RegionService regionService;
 
     /* 크루-지역 생성 */
-    public void createCrewRegion(List<RegionReqDto> regionReqDtos, Crew crew) {
+    public void createCrewRegion(List<RegionDto> regionDtos, Crew crew) {
 
-        List<CrewRegion> crewRegions = regionReqDtos.stream()
-                .map(regionReqDto -> {
-                    Region region = regionService.findRegion(regionReqDto.regionDepth1(), regionReqDto.regionDepth2(), regionReqDto.regionDepth3());
+        List<CrewRegion> crewRegions = regionDtos.stream()
+                .map(regionDto -> {
+                    Region region = regionService.findRegion(regionDto.regionDepth1(), regionDto.regionDepth2(), regionDto.regionDepth3());
                     return CrewRegion.of(region, crew);
                 }).collect(Collectors.toList());
 
         crewRegionRepository.saveAll(crewRegions);
     }
 
+    /* 특정 크루의 지역 조회 */
+    public List<RegionDto> findRegionByCrewId(Long CrewId) {
+        return crewRegionRepository.findByCrewId(CrewId).stream()
+                .map(crewRegion -> {
+                    Region region = regionService.findRegionByRegionId(crewRegion.getRegion().getId());
+                    return RegionDto.fromEntity(region);
+                })
+                .collect(Collectors.toList());
+    }
 
 }
