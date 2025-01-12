ce37e061c65aa2e4248974634d5ebb402006c782
Chaeyoon Kim <cy6kimm@gmail.com>
2025-01-12 17:13:55 +0900

    feat: joinRequest 엔티티 생성

---
+++ src/main/java/com/example/momowas/joinrequest/domain/JoinRequest.java
@@ -0,0 +1,29 @@
+package com.example.momowas.joinrequest.domain;
+
+import com.example.momowas.crew.domain.Crew;
+import com.example.momowas.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class JoinRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="crew_id")
+    private Crew crew;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
---
+++ src/main/java/com/example/momowas/joinrequest/domain/Status.java
@@ -0,0 +1,7 @@
+package com.example.momowas.joinrequest.domain;
+
+public enum Status {
+    PENDING,
+    ACCEPTED,
+    REJECTED
+}
