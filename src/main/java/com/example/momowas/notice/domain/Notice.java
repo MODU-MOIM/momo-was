ade043ccc22b71ff93b4a67d18a654a76db8a55c
Chaeyoon Kim <cy6kimm@gmail.com>
2025-01-22 15:37:06 +0900

    fix: 메서드명 및 접근 제어자 변경

--- src/main/java/com/example/momowas/notice/domain/Notice.java
+++ src/main/java/com/example/momowas/notice/domain/Notice.java
@@ -49,7 +49,7 @@ public class Notice {
     private Vote vote;
 
     @Builder
-    public Notice(String content, LocalDateTime createdAt, Crew crew, CrewMember crewMember, Vote vote) {
+    private Notice(String content, LocalDateTime createdAt, Crew crew, CrewMember crewMember, Vote vote) {
         if (!StringUtils.hasText(content)) {
             throw new IllegalArgumentException("content는 null이거나 빈 문자열이 될 수 없습니다.");
         }
@@ -59,14 +59,14 @@ public class Notice {
         this.vote= vote;
     }
 
-    public void update(String content) {
+    public void updateContent(String content) {
         if (!StringUtils.hasText(content)) {
             throw new IllegalArgumentException("content는 null이거나 빈 문자열이 될 수 없습니다.");
         }
         this.content=content;
     }
 
-    public void update(Vote vote) {
+    public void updateVote(Vote vote) {
         this.vote=Objects.requireNonNull(vote,"vote는 null이 될 수 없습니다.");
     }
 
