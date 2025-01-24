5b17f9de94377ed3f59bdcca069976875f4de0f0
Chaeyoon Kim <cy6kimm@gmail.com>
2025-01-24 23:32:19 +0900

    feat: 기록 이미지 업로드 기능 구현

--- src/main/java/com/example/momowas/record/controller/ArchiveController.java
+++ src/main/java/com/example/momowas/record/controller/ArchiveController.java
@@ -2,11 +2,14 @@ package com.example.momowas.record.controller;
 
 import com.example.momowas.record.dto.ArchiveReqDto;
 import com.example.momowas.record.service.ArchiveService;
+import com.example.momowas.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -14,6 +17,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class ArchiveController {
     private final ArchiveService archiveService;
+    private final S3Service s3Service;
 
     /* 기록 생성 */
     @PostMapping("")
@@ -23,5 +27,12 @@ public class ArchiveController {
                                             @AuthenticationPrincipal Long userId) {
         Long archiveId = archiveService.createArchive(archiveReqDto, crewId, userId);
         return Map.of("archiveId",archiveId);
+    }
+
+    /* 기록 이미지 업로드 */
+    @PostMapping("/images")
+    public Map<String, Object> uploadArchiveImage(@RequestParam("archiveImage") MultipartFile file) throws IOException {
+        String archiveImageUrl = s3Service.uploadImage(file, "archive");
+        return Map.of("archiveImageUrl", archiveImageUrl);
     }
 }
