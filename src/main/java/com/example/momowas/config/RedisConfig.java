966629bc62cfe65557fc8734a0aec2778234ee13
wlstj <22020306jinseoyeon@gmail.com>
2025-01-26 14:23:12 +0900

    chore: redis config 패키지 위치 변경

--- src/main/java/com/example/momowas/config/RedisConfig.java
+++ src/main/java/com/example/momowas/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.momowas.redis.config;
+package com.example.momowas.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
