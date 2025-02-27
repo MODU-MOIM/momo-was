61fdff1cc5959b65b82a0f983ccc910882398ff4
Chaeyoon Kim <cy6kimm@gmail.com>
2025-02-27 23:48:10 +0900

    fix: 전체 크루 멤버 조회 응답 수정

--- src/main/java/com/example/momowas/crewmember/dto/CrewMemberListResDto.java
+++ src/main/java/com/example/momowas/crewmember/dto/CrewMemberListResDto.java
@@ -4,11 +4,13 @@ import com.example.momowas.crewmember.domain.Role;
 import com.example.momowas.crewmember.domain.CrewMember;
 
 
-public record CrewMemberListResDto(String nickname,
+public record CrewMemberListResDto(Long memberId,
+                                   String nickname,
                                    String profileImage,
                                    Role role) {
     public static CrewMemberListResDto of(CrewMember crewMember) {
         return new CrewMemberListResDto(
+                crewMember.getId(),
                 crewMember.getUser().getNickname(),
                 crewMember.getUser().getProfileImage(),
                 crewMember.getRole()
