37d04d7c1e32f424989a2ce6b17b0576871d5bd3
wlstj <22020306jinseoyeon@gmail.com>
2025-03-10 20:50:27 +0900

    fix: username, password 제거

--- src/main/java/com/example/momowas/config/ElasticsearchConfig.java
+++ src/main/java/com/example/momowas/config/ElasticsearchConfig.java
@@ -1,5 +1,6 @@
 package com.example.momowas.config;
 
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -7,20 +8,15 @@ import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfigurat
 
 @Configuration
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
-    @Value("${spring.elasticsearch.username}")
-    private String username;
 
-    @Value("${spring.elasticsearch.password}")
-    private String password;
-
-    @Value("${spring.elasticsearch.uri}")
+    @Value("${spring.elasticsearch.uris}")
     private String host;
 
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
                 .connectedTo(host)
-                .withBasicAuth(username, password)
                 .build();
     }
+
 }
