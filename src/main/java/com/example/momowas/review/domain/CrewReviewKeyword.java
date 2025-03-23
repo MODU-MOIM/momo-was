4aaad503675aa8961556d5c858ee1a0a3e97da82
wlstj <22020306jinseoyeon@gmail.com>
2025-03-24 08:42:00 +0900

    fix: crew review jpa entity오류 해결

--- src/main/java/com/example/momowas/review/domain/CrewReviewKeyword.java
+++ src/main/java/com/example/momowas/review/domain/CrewReviewKeyword.java
@@ -16,11 +16,11 @@ public class CrewReviewKeyword {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "crew_assessment_id")
+    @JoinColumn(name = "crew_review_id")
     private CrewReview crewReview;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "keyword_id")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "keyword")
     private Keyword keyword;
 
     @Builder
