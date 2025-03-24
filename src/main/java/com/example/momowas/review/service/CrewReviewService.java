f541814dc2e287f558741254e5cc651a36170ac5
wlstj <22020306jinseoyeon@gmail.com>
2025-03-24 22:57:16 +0900

    fix: cron 작성 오류 수정

--- src/main/java/com/example/momowas/review/service/CrewReviewService.java
+++ src/main/java/com/example/momowas/review/service/CrewReviewService.java
@@ -117,7 +117,7 @@ public class CrewReviewService {
 
     /* 모임 일정 다음 날 자정에 크루 리뷰 요청 알림 전송 */
     @Transactional
-    @Scheduled(cron = "0 0 0 1/1 * ? *") //매일 자정마다 실행
+    @Scheduled(cron = "0 0 0 1/1 * ?")  //매일 자정마다 실행
     //@Scheduled(cron = "0 0/1 * 1/1 * ?") //1분 주기(테스트)
     public void sendCrewReviewNotification() {
         LocalDate previousDay = LocalDate.now().minusDays(1);
