c55c2664f0fe66e4f62ba5414996b9166acfdc20
Chaeyoon Kim <cy6kimm@gmail.com>
2025-03-25 14:40:33 +0900

    feat: 특정 크루 조회 응답값에 평균 별점 포함

--- src/main/java/com/example/momowas/crew/dto/CrewDetailResDto.java
+++ src/main/java/com/example/momowas/crew/dto/CrewDetailResDto.java
@@ -21,6 +21,7 @@ public record CrewDetailResDto(Long crewId,
                                Integer maxAge,
                                Gender genderRestriction,
                                String bannerImage,
+                               Double mannersRating,
                                List<RegionDto> regions,
                                LocalDateTime createdAt){
     public static CrewDetailResDto of(Crew crew, List<RegionDto> regions, Integer memberCount, CrewMember crewLeader) {
@@ -37,6 +38,7 @@ public record CrewDetailResDto(Long crewId,
                 crew.getMaxAge(),
                 crew.getGenderRestriction(),
                 crew.getBannerImage(),
+                crew.countMannersRating(),
                 regions,
                 crew.getCreatedAt()
         );
