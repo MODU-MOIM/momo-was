819d472c3e36b4235f585d8a2614654f85af121f
Chaeyoon Kim <cy6kimm@gmail.com>
2025-04-08 23:34:08 +0900

    fix: sse 연결 응답 헤더 설정

--- src/main/java/com/example/momowas/sse/controller/SseController.java
+++ src/main/java/com/example/momowas/sse/controller/SseController.java
@@ -1,6 +1,7 @@
 package com.example.momowas.sse.controller;
 
 import com.example.momowas.sse.service.SseEmitterService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +17,12 @@ public class SseController {
 
     /* 클라이언트의 이벤트 구독을 수락  */
     @GetMapping(value = "/subscribe", produces = "text/event-stream")
-    public SseEmitter subscribe(@AuthenticationPrincipal Long userId) {
+    public SseEmitter subscribe(@AuthenticationPrincipal Long userId, HttpServletResponse response) {
+        // SSE 관련 헤더 설정
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Connection", "keep-alive");
+        response.setHeader("X-Accel-Buffering", "no");
+
         return sseEmitterService.subscribe(userId);
     }
 
