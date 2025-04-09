4fc68e246407222843eed02b8e196c875e1a0600
Chaeyoon Kim <cy6kimm@gmail.com>
2025-04-09 15:16:41 +0900

    feat: cors 응답 헤더 추가

--- src/main/java/com/example/momowas/config/SecurityConfig.java
+++ src/main/java/com/example/momowas/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true); // 인증 정보를 포함한 요청 허용
-        configuration.setExposedHeaders(List.of("Authorization")); //Authorization 헤더 노출
+        configuration.setExposedHeaders(List.of("Authorization","Content-Type", "Cache-Control", "X-Accel-Buffering")); //Authorization 헤더 노출
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
